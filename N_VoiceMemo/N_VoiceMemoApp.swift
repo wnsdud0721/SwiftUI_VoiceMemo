783d7f8db25336bbd7dbc7ff317627e36cae0a58
junyoung_hong <wnsdud0721@naver.com>
2024-02-08 14:50:41 +0900

    first commit

---
+++ N_VoiceMemo/N_VoiceMemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  N_VoiceMemoApp.swift
+//  N_VoiceMemo
+//
+//  Created by Junyoung_Hong on 2024/02/08.
+//
+
+import SwiftUI
+
+@main
+struct N_VoiceMemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
